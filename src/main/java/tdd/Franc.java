c990a492e92434a5544b6e2d8da050843bb267c9
chulphan <chkim100617@gmail.com>
2020-02-17 07:37:24 +0900

    5CHF * 2 = 10CHF

---
+++ src/main/java/tdd/Franc.java
@@ -0,0 +1,18 @@
+package tdd;
+
+class Franc {
+    private int amount;
+
+    Franc(int amount) {
+        this.amount = amount;
+    }
+
+    public Franc times(int multiplier) {
+        return new Franc(this.amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Franc franc = (Franc) object;
+        return amount == franc.amount;
+    }
+}
