b70a26ffeb78c75f26ff4a794148854657387319
chulphan <chkim100617@gmail.com>
2020-02-19 12:59:00 +0900

    필요하다고 생각되는 인자(Bank) 추가, 코드-테스트 데이터 중복 제거, helper 클래스(Pair) 작성 등

--- src/test/java/tdd/AppTest.java
+++ src/test/java/tdd/AppTest.java
@@ -1,5 +1,6 @@
 package tdd;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
@@ -63,5 +64,23 @@ public class AppTest {
         Bank bank = new Bank();
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    public void testArrayEquals() {
+        assertArrayEquals(new Object[] { "abc" }, new Object[] { "abc" });
+    }
+
+    @Test
+    public void testIdentityRate() {
+        assertEquals(1, new Bank().rate("USD", "USD"));
     }
 }
