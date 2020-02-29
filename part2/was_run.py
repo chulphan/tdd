c06b23f4e400c3d540685d5e5766981577df3f05
chulphan <chkim100617@gmail.com>
2020-02-29 16:49:46 +0900

    setUp을 먼저 호출시켜 테스트 간 커플링을 걷어냈다

--- part2/was_run.py
+++ part2/was_run.py
@@ -2,8 +2,11 @@ from test_case import TestCase
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)
 
     def testMethod(self):
         self.wasRun = 1
+
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
