6b0364646cc4588af1bfe9893f60246b03fb6942
chulphan <chkim100617@gmail.com>
2020-03-01 10:36:13 +0900

    수집된 결과를 출력했다.(몇 개의 테스트가 돌아가는지) 하지만 실패한 테스트를 기록하기 위한 예외처리가 안됐다. 할 일 목록에 추가하고 다음장 부터 처리한다

--- part2/test_case.py
+++ part2/test_case.py
@@ -1,12 +1,17 @@
+from test_result import TestResult
+
 class TestCase:
     def __init__(self, name):
         self.name = name
     
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
 
     def setUp(self):
         pass
---
+++ part2/test_result.py
@@ -0,0 +1,9 @@
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+
+    def summary(self):
+        return "{} run, 0 failed".format(self.runCount)
