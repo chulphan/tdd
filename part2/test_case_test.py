14c985beaee94b39ba5e58d3b61049d19bc7c2a6
chulphan <chkim100617@gmail.com>
2020-03-01 11:08:21 +0900

    실패한 테스트를 기록하는 테스트와 구현을 했다. 이제 여러 개의 테스트 실행하는 것을 테스트하고 구현할 것이다

--- part2/test_case_test.py
+++ part2/test_case_test.py
@@ -1,4 +1,5 @@
 from test_case import TestCase
+from test_result import TestResult
 from was_run import WasRun
 
 
@@ -18,6 +19,13 @@ class TestCaseTest(TestCase):
         result = test.run()
         assert("1 run, 1 failed" == result.summary())
 
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert("1 run, 1 failed" == result.summary())
+
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
 TestCaseTest("testFailedResult").run()
+TestCaseTest("testFailedResultFormatting").run()
--- part2/test_result.py
+++ part2/test_result.py
@@ -1,9 +1,13 @@
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.failureCount = 0
 
     def testStarted(self):
         self.runCount = self.runCount + 1
 
+    def testFailed(self):
+        self.failureCount = self.failureCount + 1
+
     def summary(self):
-        return "{} run, 0 failed".format(self.runCount)
+        return "{} run, {} failed".format(self.runCount, self.failureCount)
